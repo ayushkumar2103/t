1a4d082601540d7e12d31b27c2c2d5c637a53a50
ayushkumar2103 <ayush21mar2001@gmail.com>
2024-05-08 12:11:37 +0530

    first commit

--- src/components/Error.js
+++
@@ -1,9 +0,0 @@
-const Error = ({ error = "Something went wrong...." }) => {
-  return (
-    <div className="alert alert-danger" role="alert">
-      {error}
-    </div>
-  );
-};
-
-export default Error;
